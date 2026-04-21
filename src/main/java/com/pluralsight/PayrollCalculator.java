7245e37038136ad79eb0132f3940bf088d421d4e
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-21 12:01:14 -0400

    added my try code commit

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,13 +1,34 @@
 package com.pluralsight;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
-        Scanner scanner = new scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the name of the employee file to process: ");
-        String inputFile
+        String inputFile = scanner.nextLine();
+
+        System.out.println("Enter the name of the payroll file to create: ");
+        String outputFile = scanner.nextLine();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            PrintWriter writer = new PrintWriter(new FileWriter(outputFile));
+
+            writer.println("id|name|gross pay");
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String [] parts = line.split("\\|");
+
+                int id = Integer
+
+
+            }
+        }
     }
 
     }
