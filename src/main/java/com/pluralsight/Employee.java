1ab7c4a99c7411c2b25fb0bde64f676a44b0ccb7
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-21 11:48:00 -0400

    copied and pasted the informations from the previous project.

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,37 @@
 package com.pluralsight;
 
 public class Employee {
+    public class Employee {
+
+        private int employeeId;
+        private String name;
+        private double hoursWorked;
+        private double payRate;
+
+        public Employee(int id, String n, double hours, double rate) {
+            this.employeeId = id;
+            this.name = n;
+            this.hoursWorked = hours;
+            this.payRate = rate;
+        }
+
+        public double getGrossPay() {
+            return this.hoursWorked * this.payRate;
+        }
+
+        public int getEmployeeId() {
+            return this.employeeId;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public double getHoursWorked() {
+            return this.hoursWorked;
+        }
+
+        public double getPayRate() {
+            return this.payRate;
+        }
 }
--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new scanner(System.in);
+
+        System.out.println("Enter the name of the employee file to process: ");
+        String inputFile
+    }
+
+    }
